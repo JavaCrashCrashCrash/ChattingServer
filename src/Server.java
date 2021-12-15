76a551655d7588105612396ee07d867033adcfbd
jang6129 <jang6129@naver.com>
2021-12-15 15:22:33 +0900

    upload

---
+++ src/Server.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	public static void main(String[] args) {
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		ServerSocket listener = null;
+		Socket socket = null;
+		Scanner scanner = new Scanner(System.in);
+		try {
+			listener = new ServerSocket(9999);
+			System.out.println("Waiting connection");
+			socket = listener.accept();
+
+			System.out.println("Connection Success");
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			while (true) {
+				String inputMessage = in.readLine();
+				if (inputMessage.equalsIgnoreCase("Bye")) {
+					System.out.println("Closed by client via bye message");
+					break;
+				}
+				System.out.println("Client : " + inputMessage);
+				System.out.println("Send >> ");
+				String outputMessage = scanner.nextLine();
+				out.write(outputMessage + "\n");
+				out.flush();
+			}
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				scanner.close();
+				socket.close();
+				listener.close();
+			} catch (IOException e) {
+				System.out.println("Error is occured for chating with client");
+			}
+		}
+	}
+}
